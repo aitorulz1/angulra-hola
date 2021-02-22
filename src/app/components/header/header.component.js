f764c882a41ff76f1dab3aefa737dccebb11e3b3
Aitor <aitorulz1@gmail.com>
2021-02-22 13:42:24 +0100

    Not working

---
+++ src/app/components/header/header.component.js
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: `<h1>Header component</h1>`
+})
+
+export class HeaderComponent { 
+    
+}
